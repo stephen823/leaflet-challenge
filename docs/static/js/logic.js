4d08dfc0200cf546f35f55b2d75c9a5cde63dbcb
Stephen Zhang <stephenzhang95@hotmail.com>
2020-07-26 18:30:38 -0400

    Update logic.js

--- docs/static/js/logic.js
+++ docs/static/js/logic.js
@@ -11,7 +11,7 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.streets",
-  accessToken: API_KEY
+  accessToken: "pk.eyJ1Ijoic3RlcGhlbno4MjMiLCJhIjoiY2thb2lmaGd3MGhjMzJxbzZlYnBneXJ2diJ9.P_d0zV-iNFx2vZIbjQOzqQ"
 }).addTo(map);
 
 // If data.beta.nyc is down comment out this link
